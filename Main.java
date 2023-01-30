e0d3528e849faf0d767986af44803b289bd3c049
2226374 <2226374@slu.edu.ph>
2023-01-30 11:54:04 +0800

    continued Main menu finished read.java

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    public Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        String csvFile = "D:\\hghgfh\\src\\Prelim\\Exercise\\dataset\\src\\Chess Raw Data Sets.csv";
-        Read.read(csvFile);
+        System.out.println("The purpose of this application is to manage data from an external file."
+                + "\n The processes that this ");
+    }
+
+    public void pressEnter() {
+        System.out.println("Press ENTER to go back...");
+        sc.next();
+    }
+
+    public void mainMenu() {
+        switch (){
+            case 1:
+        }
+    }
+
+    public void enterChoice(){
 
     }
+
+
 }
