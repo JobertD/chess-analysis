37e2bd84316506c34f39723fc0778f17a251e1ca
Marko <2212093@slu.edu.ph>
2023-01-30 11:28:13 +0800

    added hashmap

--- Read.java
+++ Read.java
@@ -1,7 +1,20 @@
 import java.io. * ;
+import java.util.HashMap;
+
 public class Read {
     public static final String delimiter = ",";
     public static void read(String csvFile) {
+        HashMap <String, Integer> category = new HashMap<>();
+        category.put("Black", 0 );
+        category.put("BlackElo", 1);
+        category.put("White", 2);
+        category.put("WhiteElo", 3);
+        category.put("Date", 4);
+        category.put("Event", 5);
+        category.put("First Move", 6);
+        category.put("Category", 7);
+        category.put("Weekday", 8);
+
         try {
             BufferedReader br = new BufferedReader(new FileReader(csvFile));
             String line = " ";
@@ -15,7 +28,7 @@ public class Read {
                     data[row][x] = chess[x];
                 }
                 System.out.println(data[row][2] + "");
-                System.out.println(data[row][4] + "");
+
                 row++;
             }
 
