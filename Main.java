7e5acc5412f700c25727f6fb4445bc76a33bdf68
Jobert  Deluna <2226374@slu.edu.ph>
2023-01-30 03:06:41 +0000

    Upload New File

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String csvFile = "D:\\hghgfh\\src\\Prelim\\Exercise\\dataset\\src\\Chess Raw Data Sets.csv";
+        Read.read(csvFile);
+
+    }
+}
