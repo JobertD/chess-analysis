f867b7a5dd0151d3f9c58867769fe64bde70b99a
Ian Cristino <2224378@slu.edu.ph>
2023-02-01 17:56:09 +0800

    finished searching class.
    searching class: allows user to search for a match or details of a match in the database.

--- Searching.java
+++ Searching.java
@@ -1,2 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.*;
+
 public class Searching {
+
+    public static final String delimiter = ",";
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String input = "";
+        int count = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("Chess_Raw_Data_Sets.csv"));
+            String line;
+            String[] chess;
+            String data[][] = new String[1001][9];
+            int row = 0;
+
+            while ((line = br.readLine()) != null) {
+                chess = line.split(delimiter);
+                for (int x = 0; x <= chess.length - 1; x++) {
+                    data[row][x] = chess[x];
+                }
+                row++;
+            }
+
+            input = in.nextLine();
+            for(int i = 0; i < 1001; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (input.equals(data[i][j])){
+                        count++;
+                        System.out.println(Arrays.deepToString(data[i]));
+                    }
+                }
+            }
+            if (count == 0) {
+                System.out.println("Username is not found in the database.");
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
